cf532407ff2aef9e234e9c185bac005bf41c90ec
Renfei Song <renfei.song@gmail.com>
2014-08-11 23:22:44 +0800

    update Three.js, r60 -> r65

--- index.php
+++ index.php
@@ -9,46 +9,12 @@ require_once "functions.php";
 get_header("Dimensions");
 ?>
 
-<style>
-    body {
-        font-family: Monospace;
-        background-color: #f0f0f0;
-        margin: 0px;
-        overflow: hidden;
-    }
+<div id="circles">
 
-    #circles {
-        -webkit-filter: blur(10px);
-        -moz-filter: blur(10px);
-        -o-filter: blur(10px);
-        -ms-filter: blur(10px);
-        filter: blur(10px);
-    }
+</div>
 
-    #no-blur {
-        font-size:100px;
-        -webkit-filter: blur(0px); !important
-        -moz-filter: blur(0px); !important
-        -o-filter: blur(0px); !important
-        -ms-filter: blur(0px); !important
-        filter: blur(0px); !important
-    }
-</style>
-</head>
-
-<body>
-
-    <div id="circles">
-
-        <div id="no-blur">
-            <p>no blur</p>
-        </div>
-
-    </div>
-		<script src="three.min.js"></script>
-		<script src="stats.min.js"></script>
-        <script src="animation.js"></script>
+<script src="lib/webgl/Three.js"></script>
+<script src="lib/webgl/index-animation.js"></script>
 
 <?php
 get_footer();
-?>
