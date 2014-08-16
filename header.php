d5e05d9d1763bf444883912b6891143bb678b7c4
Renfei Song <renfei.song@gmail.com>
2014-08-16 19:49:14 +0800

    various bug fixes

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
  * Displays all of the <head> section and everything up till <div id="centrum" class="site-main">
  *
  * @author Renfei Song
+ * @author Qin Bingchen
  */
 
 require_once "functions.php";
@@ -14,23 +15,8 @@ global $user_controller;
 $user_controller = new UserController();
 $user_controller->init();
 
-switch(basename($_SERVER['SCRIPT_NAME'])) {
-    case "browse.php":
-        $browse_url = "#";
-        $browse_status = "current";
-        break;
-    case "upload.php":
-        $upload_url = "#";
-        $upload_status = "current";
-        break;
-    case "my-profile.php":
-        $profile_url = "#";
-        $profile_status = "current";
-        break;
-}
-
 ?><!DOCTYPE html>
-<html lang="zh-Hans-CN">
+<html lang="zh-cmn-Hans">
 <head>
     <meta charset="UTF-8">
     <title><?php echo $__page_title ?></title>
@@ -41,45 +27,41 @@ switch(basename($_SERVER['SCRIPT_NAME'])) {
     <link href="lib/core/bootstrap/v3-icons/bootstrap-fonts.css" rel="stylesheet" media="screen">
     <link href="lib/core/components.css" rel="stylesheet" media="screen">
     <link href="lib/core/style.css" rel="stylesheet" media="screen">
-    <script src="lib/webgl/Three-r68.min.js"></script>
 </head>
 
 <body>
-
 <div id="wrapper" class="<?php echo basename($_SERVER['SCRIPT_NAME'], ".php"); ?>">
-                <header id="masthead" class="site-header">
-<div class="inner">
-    <div class="site-title">
-        <a href="index.php"><img class="site-logo" src="lib/img/logo.png"></a>
-        <a href="index.php" class="site-title-text">Dimensions</a>
-    </div>
-    <div class="site-search"></div>
-    <div class="site-navigation">
-        <ul>
-            <li><a href='browse.php'>浏览</a></li>
-        </ul>
-    </div>
-    <div class="site-navigation right">
-        <ul>
-            <?php if ($user_controller->has_user): ?>
-                <li class="create"><a href="editor/create.php">新建</a></li>
-                <li class="<?php echo @$upload_status?>"><a href="<?php if (isset($upload_url)) echo $upload_url; else echo 'upload.php'?>">上传模型</a></li>
-                <li class="dropdown">
-                    <a data-toggle="dropdown" href="#"><?php echo $user_controller->username ?><span class="glyphicon glyphicon-chevron-down"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="<?php if (isset($profile_url)) echo $profile_url; else echo 'my-profile.php'?>"><span class="glyphicon glyphicon-user"></span>个人中心</a></li>
-                        <li><a href="prefortobemodified.php"><span class="glyphicon glyphicon-cog"></span>设定</a></li>
-                        <li><a href="logout.php"><span class="glyphicon glyphicon-off"></span>登出</a></li>
-                    </ul>
-                </li>
-            <?php else: ?>
-                <li><a href='login.php'>登录</a></li>
-                <li><a href='signup.php' class="inverted-button">注册</a></li>
-            <?php endif; ?>
-        </ul>
-    </div>
-</div>
-</header>
+    <header id="masthead" class="site-header">
+        <div class="inner">
+            <div class="site-title">
+                <a href="index.php" title="Dimensions">
+                    <img class="site-logo" src="lib/img/logo.png">
+                </a>
+                <a href="index.php" title="Dimensions" class="site-title-text">Dimensions</a>
+            </div>
+            <div class="site-search"></div>
+            <div class="site-navigation">
+                <ul>
+                    <li class="browse"><a href="browse.php" title="浏览所有模型">浏览</a></li>
+                    <?php if ($user_controller->has_user): ?>
+                        <li class="create"><a href="editor/create.php">新建</a></li>
+                        <li class="upload"><a href="upload.php">上传</a></li>
+                        <li class="dropdown">
+                            <a data-toggle="dropdown" data-target="#" href="user.php"><?php echo $user_controller->username ?><span class="glyphicon glyphicon-chevron-down"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="user.php" title="个人中心"><span class="glyphicon glyphicon-user"></span>个人中心</a></li>
+                                <li><a href="settings.php" title="用户设定"><span class="glyphicon glyphicon-cog"></span>设定</a></li>
+                                <li><a href="logout.php" title="登出"><span class="glyphicon glyphicon-off"></span>登出</a></li>
+                            </ul>
+                        </li>
+                    <?php else: ?>
+                        <li><a href='login.php'>登录</a></li>
+                        <li><a href='signup.php' class="inverted-button">注册</a></li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+        </div>
+    </header>
 
-<div id="centrum" class="site-main">
-    <div class="site-content">
+    <div id="centrum" class="site-main">
+        <div class="site-content">
--- index.php
+++ index.php
@@ -3,48 +3,41 @@
  * The initial page of Dimensions.
  *
  * @author Renfei Song
+ * @author Qin Bingchen
  */
 
 require_once "functions.php";
 
 get_header("Dimensions"); ?>
 
-    <link href="lib/core/index.css" rel="stylesheet" media="screen">
-    <div class="section-one">
-        <div class="intro-video" id="intro-video">
-        </div>
-        <div class="intro-title">
+    <section class="billboard">
+        <div class="hero"></div>
+        <div class="caption">
             <p>创建并分享动态3D模型</p>
         </div>
-    </div>
+    </section>
 
-    <div class="section-two">
+    <section class="features">
         <div class="feature-list">
-            <div class="feature">
+            <div class="item">
                 <div class="feature-title">Upload in seconds</div>
                 <div class="feature-content">
                     Sketchfab lets you upload your models without limit, either via your web browser or directly from your 3D editor.
                 </div>
             </div>
-            <div class="feature">
+            <div class="item">
                 <div class="feature-title">Upload in seconds</div>
                 <div class="feature-content">
                     Sketchfab lets you upload your models without limit, either via your web browser or directly from your 3D editor.
                 </div>
             </div>
-            <div class="feature">
+            <div class="item">
                 <div class="feature-title">Upload in seconds</div>
                 <div class="feature-content">
                     Sketchfab lets you upload your models without limit, either via your web browser or directly from your 3D editor.
                 </div>
             </div>
         </div>
-    </div>
-
-    <div class="section-three">
-
-    </div>
-    <script src="lib/webgl/index-animation.js"></script>
-
+    </section>
 <?php
 get_footer();
